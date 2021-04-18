defacaa86bc4efebd3924b4389976c599a719c21
sabbagh99 <a.sameer1999@gmail.com>
2021-04-18 20:45:26 +0300

    add lab 11

---
+++ src/test/java/com/songr/songr/SongrApplicationTests.java
@@ -0,0 +1,21 @@
+package com.songr.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.SpringBootTest;
+import  static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		Album siaAlbum = new Album("This Is Acting","Sia",3,1354,"https://www.colendri.lt/media/EshopProducts/150/Sia-This-Is-Acting-CD.jpg");
+			@assertThat();
+
+	}
+
+}
